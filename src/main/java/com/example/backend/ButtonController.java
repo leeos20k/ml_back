f27c38373719be726ae542d09a04cede0fc107d3
leeos20k <leeos20k@naver.com>
2023-09-19 09:39:21 +0900

    first commit

---
+++ src/main/java/com/example/backend/ButtonController.java
@@ -0,0 +1,15 @@
+package com.example.backend;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ButtonController {
+
+    @PostMapping("/echo")
+    public String echoButtonName(@RequestBody String buttonName) {
+        System.out.println("Received Button Name: " + buttonName);
+        return "Received and echoed: " + buttonName;
+    }
+}
