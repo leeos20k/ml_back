c301394a6e6a7f5ff31f2779401d917bd87374a8
조경제 <jskytorod@gmail.com>
2023-10-11 11:36:52 +0900

    231011

---
+++ src/main/java/com/example/backend/Repository/SearchLandInfoRepository.java
@@ -0,0 +1,13 @@
+package com.example.backend.Repository;
+
+
+import com.example.backend.Entity.LandInfo;
+import com.example.backend.Entity.SearchLandInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SearchLandInfoRepository extends JpaRepository<SearchLandInfo, Long> {
+
+
+}
